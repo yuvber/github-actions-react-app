1afdfe2b4cfac04c3ae015c6fc371afbf6871402
Yuval Berkman <yuvalberkman@Yuvals-MacBook-Air.local>
2022-05-25 15:08:06 +0300

    fix: fix

--- release.config.js
+++ release.config.js
@@ -4,6 +4,11 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer', 
         '@semantic-release/release-notes-generator', 
-        '@semantic-release/github'
+        ['@semantic-release/github', {
+            assets: [
+                { path: "build.zip", label: "build"},
+                { path: "coverage.zip", label: "coverage"}
+            ]
+        }]
     ]
 }
