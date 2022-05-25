473b159f64680c3d15f74322d8ef1fa7123bf092
Yuval Berkman <yuvalberkman@Yuvals-MacBook-Air.local>
2022-05-25 14:37:17 +0300

    feat:add semantic release

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.export = {
+    branches: "master",
+    repositoryUrl: "https://github.com/yuvber/github-actions-react-app",
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github'
+    ]
+}
